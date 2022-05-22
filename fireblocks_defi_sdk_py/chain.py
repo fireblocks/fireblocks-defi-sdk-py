77d51cf5cac0a30cf67ddf5afe2f0b31b2fd5171
tomerlp44 <tomerlp44@gmail.com>
2022-05-22 18:24:06 +0300

    refactor dirs

---
+++ fireblocks_defi_sdk_py/chain.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class Chain(Enum):
+    MAINNET = "mainnet"
+    ROPSTEN = "ropsten"
+    KOVAN = "kovan"
+    BSC = "bsc"
+    BSC_TEST = "bsc_test"
+    POLYGON = "polygon"
