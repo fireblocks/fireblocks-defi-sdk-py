ca8f095c2103f5e27adfc9e102125056e8d79c2a
Yanir Levi <janlev@janlevs-MacBook-Pro.local>
2022-06-22 20:18:05 +0300

    Finalized examples

--- fireblocks_defi_sdk_py/nft/examples/multi_token_example.py
+++ fireblocks_defi_sdk_py/nft/examples/multi_token_example.py
@@ -16,7 +16,7 @@ TOKEN_IDS = [1, 2]
 if __name__ == "__main__":
     # We start by building a bridge to the Kovan network, including initiating a ERC1155 object.
     kovan_bridge = Web3Bridge(SDK, VAULT_ID, CONTRACT, Chain.KOVAN)
-    multi_token_contract = ERC1155(kovan_bridge)
+    multi_token_contract_bridge = ERC1155(kovan_bridge)
 
     # ERC1155 implements all the basic capabilities mentioned in ERC721:
     # https://ethereum.org/en/developers/docs/standards/tokens/erc-1155/
@@ -24,12 +24,13 @@ if __name__ == "__main__":
 
     # We will check the balance of tokens 1, 2 under the vault wallet address, and then use it to send all the tokens to
     # another address.
-    tokens_balance = multi_token_contract.balance_of_batch(TOKEN_IDS)
+    tokens_balance = multi_token_contract_bridge.balance_of_batch(TOKEN_IDS)
     if tokens_balance:
         # Once again, the object will by default attempt to send assets from the given wallet address, unless defined
         # otherwise. We can also add a note to the transaction in Fireblocks.
-        transfer_tokens = multi_token_contract.safe_batch_transfer_from(RECEIVER_ADDRESSES, TOKEN_IDS, tokens_balance,
-                                                                        note="Transfer to another vault.")
+        transfer_tokens = multi_token_contract_bridge.safe_batch_transfer_from(RECEIVER_ADDRESSES, TOKEN_IDS,
+                                                                               tokens_balance,
+                                                                               note="Transfer to another vault.")
         transfer_result = kovan_bridge.check_tx_is_completed(transfer_tokens['id'])
 
         if transfer_result == TRANSACTION_STATUS_COMPLETED:
--- fireblocks_defi_sdk_py/nft/examples/nft_example.py
+++ fireblocks_defi_sdk_py/nft/examples/nft_example.py
@@ -15,7 +15,7 @@ TOKEN_ID = 1
 if __name__ == "__main__":
     # We start by building a bridge to the Ropsten network, including initiating a ERC721 object.
     ropsten_bridge = Web3Bridge(SDK, VAULT_ID, CONTRACT_ADDRESS, Chain.ROPSTEN)
-    nft_contract = ERC721(ropsten_bridge)
+    nft_contract_bridge = ERC721(ropsten_bridge)
 
     # ERC721 implements all the basic capabilities mentioned in ERC721:
     # https://ethereum.org/en/developers/docs/standards/tokens/erc-721/
@@ -23,28 +23,28 @@ if __name__ == "__main__":
 
     # Each interface has a unique ID. If no parameter is passed onto the function, it checks for the default interface
     # of token, meaning - 721.
-    interface_supported = nft_contract.supports_interface()
+    interface_supported = nft_contract_bridge.supports_interface()
     if interface_supported:
-        print(f"{nft_contract} - supports ERC721 interface.")
+        print(f"{nft_contract_bridge} - supports ERC721 interface.")
 
     # Next, we will validate whether we own a balance of NFT, and attempt to send a Token to another address.
     # We can use the balance_of function, and if we don't pass any arguments to it, it will use the relevant wallet
     # address to the vault ID passed to the Web3Bridge object.
-    balance = nft_contract.balance_of()
-    print(f"{nft_contract.wallet_address} owns {balance} tokens.")
+    balance = nft_contract_bridge.balance_of()
+    print(f"{nft_contract_bridge.wallet_address} owns {balance} tokens.")
 
     if balance:
         # We will now verify if the owner of the token is indeed the wallet, and then transfer it to another address.
-        token_owner = nft_contract.owner_of(TOKEN_ID)
-        if token_owner == nft_contract.wallet_address:
-            receiver_initial_balance = nft_contract.balance_of(RECEIVER_ADDRESS)
+        token_owner = nft_contract_bridge.owner_of(TOKEN_ID)
+        if token_owner == nft_contract_bridge.wallet_address:
+            receiver_initial_balance = nft_contract_bridge.balance_of(RECEIVER_ADDRESS)
             # safe_transfer_from returns a dictionary with a status and id.
-            transaction = nft_contract.safe_transfer_from(RECEIVER_ADDRESS, TOKEN_ID)
+            transaction = nft_contract_bridge.safe_transfer_from(RECEIVER_ADDRESS, TOKEN_ID)
             transaction_result = ropsten_bridge.check_tx_is_completed(transaction['id'])
 
             if transaction_result == TRANSACTION_STATUS_COMPLETED:
                 # After the transaction has been completed we check whether the balance of the receiver has been updated
-                if nft_contract.balance_of(RECEIVER_ADDRESS) == receiver_initial_balance + 1:
+                if nft_contract_bridge.balance_of(RECEIVER_ADDRESS) == receiver_initial_balance + 1:
                     print(f"{RECEIVER_ADDRESS} has received the token successfully.")
             else:
                 print("Could not post transaction.")
