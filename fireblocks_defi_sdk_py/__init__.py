c12dcccbeef2ee1175fbcbdff08c1626d8250bf1
Yanir Levi <janlev@janlevs-MacBook-Pro.local>
2022-07-21 11:30:35 +0300

    Updated deployment option

--- fireblocks_defi_sdk_py/__init__.py
+++ fireblocks_defi_sdk_py/__init__.py
@@ -1,3 +1,3 @@
 from .web3_bridge import Web3Bridge
 from .chain import Chain
-from .tokenization import BaseToken, ERC721, ERC1155, CustomToken, fetch_abi
+from .tokenization import BaseToken, ERC721, ERC1155, CustomToken, fetch_abi, ContractDeployer
--- fireblocks_defi_sdk_py/tokenization/__init__.py
+++ fireblocks_defi_sdk_py/tokenization/__init__.py
@@ -1,2 +1,3 @@
 from .utils.helpers import fetch_abi
 from .tokens import BaseToken, ERC721, ERC1155, CustomToken
+from .contract_deployer import ContractDeployer
